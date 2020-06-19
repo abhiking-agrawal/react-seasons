e0495a7cba75ce3879d1e866273f0e3feb5bc9a3
Abhijeet <dev1.parsa@gmail.com>
2020-06-19 10:38:13 -0400

    alternate state initialization

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import Season from './Season'
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    
-    //direct assignment just one time
-    this.state = {lat:null, errMsg: null };
-    
-  }
-
+  state = {lat:null, errMsg: null };
   componentDidMount(){
     console.log('My component was rendered')
     window.navigator.geolocation.getCurrentPosition(
